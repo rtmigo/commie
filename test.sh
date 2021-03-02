516b512817e51bf6549e7a50f5b38ce4ce2749d5
rtmigo <art.galkin@gmail.com>
2021-03-02 23:36:57 +0300

    automation

--- test.sh
+++ test.sh
@@ -4,5 +4,5 @@ set -e
 thisFileParentDir="$(dirname "$(perl -MCwd -e 'print Cwd::abs_path shift' "$0")")"
 cd "$thisFileParentDir"
 
-# nosetests --all-modules
-python3 setup.py test
+nosetests --all-modules
+# python3 setup.py test

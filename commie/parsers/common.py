5dd21db782463aa28cd2a0d24fc5e589b94b25f3
rtmigo <art.galkin@gmail.com>
2021-03-02 23:33:35 +0300

    test for Comment

--- commie/parsers/common.py
+++ commie/parsers/common.py
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: Copyright (c) 2021 Art Galkin <ortemeo@gmail.com>
 # SPDX-FileCopyrightText: Copyright (c) 2015 Jean-Ralph Aviles
 # SPDX-License-Identifier: MIT
-
+import unittest
 from typing import NamedTuple, Optional
 
 
@@ -57,10 +57,21 @@ class Comment:
 
 
 	def __repr__(self):
-		return f"Comment({self.code_span}, {self.text_span}, {self.multiline})"
+		return f"Comment({repr(self.source)}, {self.code_span}, {self.text_span}, {self.multiline})"
 
 	def __eq__(self, other):
 		if isinstance(other, self.__class__):
 			if self.__dict__ == other.__dict__:
 				return True
 		return False
+
+
+class TestComment(unittest.TestCase):
+	def test(self):
+
+		c = Comment("Code /* commented */ like that", Span(5,20), Span(7,18), True)
+		self.assertEqual(c.code, "/* commented */")
+		self.assertEqual(c.text, " commented ")
+		self.assertEqual(repr(c), "Comment('Code /* commented */ like that', Span(start=5, end=20), Span(start=7, end=18), True)")
+
+

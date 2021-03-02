4a2306d79c6e0ae66a86a330020f1c4242ecffb0
rtmigo <art.galkin@gmail.com>
2021-03-02 20:49:28 +0300

    samples

--- example/example_iter_comments.py
+++ example/example_iter_comments.py
@@ -10,9 +10,9 @@ sourceCode = file.read_text()
 for comment in commie.iter_comments(file.read_text(), file.name):
 
   # comment code: "/* sample */"
-  print("Comment inner text:", comment.text)
-  print("Comment text location:", comment.text_span.start, comment.text_span.end)
-
-  # comment text: " sample "
   print("Comment code:", comment.code)
   print("Comment code location:", comment.code_span.start, comment.code_span.end)
+
+  # comment text: " sample "
+  print("Comment inner text:", comment.text)
+  print("Comment text location:", comment.text_span.start, comment.text_span.end)
--- example/example_iter_comments_file.py
+++ example/example_iter_comments_file.py
@@ -8,9 +8,9 @@ file = Path(__file__).parent/"data"/"multiply.go"
 for comment in commie.iter_comments_file(file):
 
   # comment code: "/* sample */"
-  print("Comment inner text:", comment.text)
-  print("Comment text location:", comment.text_span.start, comment.text_span.end)
-
-  # comment text: " sample "
   print("Comment code:", comment.code)
   print("Comment code location:", comment.code_span.start, comment.code_span.end)
+
+  # comment text: " sample "
+  print("Comment inner text:", comment.text)
+  print("Comment text location:", comment.text_span.start, comment.text_span.end)

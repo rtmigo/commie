5eafa8feae2168a50fb5e460fd16b116f5325a37
rtmigo <art.galkin@gmail.com>
2021-03-03 06:21:24 +0300

    added single line comments grouping

--- commie/__init__.py
+++ commie/__init__.py
@@ -1,4 +1,5 @@
 from ._01_common import Comment, Span
 from ._01_errors import *
 from ._02_detector import iter_comments_str, iter_comments_file, iter_comments
+from ._03_glue import group_singleline_comments
 from .parsers import *

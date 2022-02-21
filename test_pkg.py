a212eac5aa1474506dff5e0e6835641ba911a2fb
Artem IG <ortemeo@gmail.com>
2022-02-21 22:07:30 +0300

    upd

--- test_pkg.py
+++ test_pkg.py
@@ -2,7 +2,7 @@ from chkpkg import Package
 
 if __name__ == "__main__":
     with Package() as pkg:
-        pkg.run_python_code('import commie')
+        pkg.run_python_code('from commie import iter_comments_go')
 
     print("\nPackage is OK!")
 
